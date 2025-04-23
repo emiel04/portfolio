a2b296f61d2886def415da8036f3185d568c6770
emiel <emielde04@gmail.com>
2025-04-23 21:23:00 +0200

    updated site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,7 +7,7 @@ import cloudflare from '@astrojs/cloudflare';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://emiel04.5139512.xyz/',
   integrations: [mdx(), sitemap()],
   adapter: cloudflare(),
 });
