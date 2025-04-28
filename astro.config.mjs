f95ae1de2f3ae5f1fd85c4930210e6f4a9cca247
emiel <emielde04@gmail.com>
2025-04-29 00:01:42 +0200

    remove react

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,11 @@ import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import tailwindcss from "@tailwindcss/vite";
-import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://emiel04.5139512.xyz/',
-  integrations: [mdx(), sitemap(), react()],
+  integrations: [mdx(), sitemap()],
   vite: {
     plugins: [tailwindcss()],
   },
