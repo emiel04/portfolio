0e6c92ceec9f285795306f41d4dfc03c2e4864a2
emiel <emielde04@gmail.com>
2025-04-29 00:14:53 +0200

    format

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,14 +1,14 @@
 // @ts-check
-import { defineConfig } from 'astro/config';
-import mdx from '@astrojs/mdx';
-import sitemap from '@astrojs/sitemap';
+import { defineConfig } from "astro/config";
+import mdx from "@astrojs/mdx";
+import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://emiel04.5139512.xyz/',
-  integrations: [mdx(), sitemap()],
-  vite: {
-    plugins: [tailwindcss()],
-  },
+	site: "https://emiel04.5139512.xyz/",
+	integrations: [mdx(), sitemap()],
+	vite: {
+		plugins: [tailwindcss()],
+	},
 });
--- src/consts.ts
+++ src/consts.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "Emiel Delaey - Software Engineer";
-export const SITE_DESCRIPTION = 'Portfolio of Emiel Delaey, professional Software Engineer.';
+export const SITE_DESCRIPTION = "Portfolio of Emiel Delaey, professional Software Engineer.";
 export const LINKEDIN_URL = "https://www.linkedin.com/in/emield/";
 export const GITHUB_URL = "https://www.github.com/emiel04";
