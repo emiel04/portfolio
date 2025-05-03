bff75e8f332e91b91a0c344beeb40f4e66a8c0b8
emiel <emielde04@gmail.com>
2025-05-03 12:43:30 +0200

    icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,11 +3,11 @@ import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwindcss from "@tailwindcss/vite";
-
+import icon from "astro-icon";
 // https://astro.build/config
 export default defineConfig({
 	site: "https://5139512.xyz/",
-	integrations: [mdx(), sitemap()],
+	integrations: [mdx(), sitemap(), icon()],
 	vite: {
 		plugins: [tailwindcss()],
 	},
